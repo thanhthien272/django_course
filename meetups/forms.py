0c6fe8ff7eaa97aad0a783e0a71b0b3abd2cb533
thanhthien272 <ctthanh1999@gmail.com>
2021-09-29 20:13:23 +0700

    use new form by Django

--- meetups/forms.py
+++ meetups/forms.py
@@ -1,10 +1,6 @@
 from django import forms
 from django.forms import fields
 
-from .models import Participant
 
-
-class RegistrationForm(forms.ModelForm):
-    class Meta:
-        model = Participant
-        fields = ['email']
+class RegistrationForm(forms.Form):
+    email = forms.EmailField(label='Your email')
